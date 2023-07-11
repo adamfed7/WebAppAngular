d47aa7b7d548ea13b3bc92c7ef944d1163d6e6dd
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-07-11 20:48:57 +0200

    Routing configuration

--- Manage/src/app/app-routing.module.ts
+++ Manage/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { FunctionalityDetailsComponent } from './functionality-details/functionality-details.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/projects', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'projects/:id', component: ProjectDetailsComponent },
+  { path: 'projects/:id/functionality/:id', component: FunctionalityDetailsComponent },
+  { path: 'projects/:id/functionality/:id/task/:id', component: TaskDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
