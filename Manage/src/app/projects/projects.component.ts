efae876e36e68625ff3b6408fdac494fb290f71b
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-07-11 20:52:55 +0200

    Project component inplementation

--- Manage/src/app/projects/projects.component.ts
+++ Manage/src/app/projects/projects.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  projects: Project[] = [{
+    name: 'Default Project',
+    description: 'This is a default project'
+  }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
+
