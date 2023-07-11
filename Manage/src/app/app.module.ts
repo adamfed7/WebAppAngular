d06915dae5a7b74b3a03eacf473c2c21a61494dc
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2023-07-11 20:35:07 +0200

    Generate components

--- Manage/src/app/app.module.ts
+++ Manage/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { FunctionalityDetailsComponent } from './functionality-details/functionality-details.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ProjectDetailsComponent,
+    FunctionalityDetailsComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
